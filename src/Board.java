027cf53fa9433387cfcd66a43b15d40d141408d9
Victor Lundgren <victor.lundgren@outlook.com>
2020-11-11 11:28:25 +0100

    added Board, Disc and Color classes

---
+++ src/Board.java
@@ -0,0 +1,15 @@
+public class Board {
+    private Disc[][] board;
+
+    public Board(Disc[][] board) {
+        this.board = board;
+    }
+
+    public Disc[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(Disc[][] board) {
+        this.board = board;
+    }
+}
---
+++ src/Disc.java
@@ -0,0 +1,15 @@
+public class Disc {
+    private Color color;
+
+    public Disc(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
