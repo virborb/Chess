31afc9e3722de7e03f9022ab8615eba93b40c007
Victor Lundgren <victor.lundgren@outlook.com>
2020-11-12 09:20:56 +0100

    implementet contructor and added GetDisc

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,16 @@
 public class Board {
     private Disc[][] board;
 
-    public Board(Disc[][] board) {
-        this.board = board;
+    public Board() {
+        this.board = new Disc[8][8];
+        this.board[4][4].setColor(Color.WHITE);
+        this.board[4][5].setColor(Color.BLACK);
+        this.board[5][4].setColor(Color.BLACK);
+        this.board[5][5].setColor(Color.WHITE);
+    }
+
+    public Disc GetDisc(int x, int y) {
+        return board[x][y];
     }
 
     public Disc[][] getBoard() {
