4533a459d7c9202ddb3f80d1ff6106270d58d809
Victor Lundgren <victor.lundgren@outlook.com>
2020-11-12 09:25:00 +0100

    change Main to Othello and Added Class Rules

--- src/Othello.java
+++ src/Othello.java
@@ -1,6 +1,7 @@
-public class Main {
+public class Othello {
 
     public static void main(String[] args) {
-
+        Board board = new Board();
+        System.out.println(board.getBoard());
     }
 }
