2c4ca8eaed9a661c165be340f9f6cae13179d256
Victor Lundgren <victor.lundgren@outlook.com>
2020-11-23 13:19:22 +0100

    Added a contrutor which take an int array with position

--- src/model/Position.java
+++ src/model/Position.java
@@ -12,6 +12,17 @@ public class Position {
         this.col = col;
     }
 
+    /**
+     * constructor which take an int array where
+     * the first index is the row and the second
+     * index is the column.
+     * @param position An Array with the position.
+     */
+    public Position(int[] position) {
+        this.row = position[0];
+        this.col = position[1];
+    }
+
     public int getRow() {
         return row;
     }
