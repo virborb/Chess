eca2a00e148230f5f708f0fc8b19308d2b96ddb9
Victor Lundgren <victor.lundgren@outlook.com>
2020-12-03 13:01:09 +0100

    implementet getMoves and test for it

--- src/model/pieces/King.java
+++ src/model/pieces/King.java
@@ -1,5 +1,6 @@
 package model.pieces;
 
+import controller.ChessController;
 import model.Color;
 import model.Position;
 
@@ -17,7 +18,27 @@ public class King extends Piece {
 
     @Override
     public ArrayList<Position> getMoves() {
-        return null;
+        ArrayList<Position> positions = new ArrayList<>();
+        Position start = this.getPosition();
+        int startRow = start.getRow();
+        int startCol = start.getCol();
+        Position[] directions = new Position[8];
+        directions[0] = new Position(startRow-1,startCol+1);
+        directions[1] = new Position(startRow+1,startCol+1);
+        directions[2] = new Position(startRow-1,startCol-1);
+        directions[3] = new Position(startRow+1,startCol);
+        directions[4] = new Position(startRow-1,startCol);
+        directions[5] = new Position(startRow,startCol-1);
+        directions[6] = new Position(startRow,startCol+1);
+        directions[7] = new Position(startRow+1,startCol-1);
+        for (Position direction:directions) {
+            int row = direction.getRow();
+            int col = direction.getCol();
+            if (row >= 0 && row < ChessController.ROWS && col >= 0 && col < ChessController.COLUMNS) {
+                positions.add(direction);
+            }
+        }
+        return positions;
     }
 
     @Override
---
+++ src/test/pieces/KingTest.java
@@ -0,0 +1,34 @@
+package model.pieces;
+
+import model.Board;
+import model.Position;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KingTest {
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+        board.initBoard();
+    }
+
+    @Test
+    void testGetMoves() {
+        Piece piece = board.GetPiece(new Position(0, 4));
+        ArrayList<Position> positions = piece.getMoves();
+        ArrayList<Position> expected = new ArrayList<>();
+        expected.add(new Position(0, 3));
+        expected.add(new Position(0, 5));
+        expected.add(new Position(1, 3));
+        expected.add(new Position(1, 4));
+        expected.add(new Position(1, 5));
+        assertTrue(positions.containsAll(expected) && positions.size() == 5);
+    }
+}
